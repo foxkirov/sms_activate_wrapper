9028f896fe5d3c47e256af27fd841d21c102f8aa
Anton <Penchinskikh>
2022-04-30 13:39:31 +0300

    created package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='pysmsactivate',
+    version='0.1',
+    description='Sms Activate Wrapper Package',
+    url='http://example.com',
+    install_requires=['smsactivate==1.3'],
+    packages=['pysmsactivate'],
+)
