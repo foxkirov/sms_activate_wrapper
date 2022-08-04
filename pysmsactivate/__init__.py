fd69f8e054f90211248bfed2778e4eb82801c6b2
anton <foxkirov@mail.ru>
2022-08-04 17:53:35 +0300

    small fix

--- pysmsactivate/__init__.py
+++ pysmsactivate/__init__.py
@@ -1,2 +1,9 @@
+# -*- coding: utf-8 -*-
+"""sms activating wrapper"""
+
+__version__ = "0.1.0"
+__author__ = "Antony Penchinski"
+__license__ = "LGPL v3"
+
 from .__main__ import Activation
 __all__ = ["Activation"]
--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 
 setup(
     name='pysmsactivate',
-    version='0.1',
+    version='0.1.0',
     description='Sms Activate Wrapper Package',
-    url='http://example.com',
-    install_requires=['smsactivate==1.3'],
+    url='https://github.com/foxkirov/sms_activate_wrapper',
+    install_requires=['smsactivate==1.3', 'requests==2.27.1'],
     packages=['pysmsactivate'],
 )
